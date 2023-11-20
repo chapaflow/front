d2d718be8f18507ff40dd4bc89aff688b8213b92
chapa <83264709+chapaflow@users.noreply.github.com>
2023-11-20 15:46:40 +0600

    Update script.js

--- JavaScript/smth new/управление HTML и обработка форм/script.js
+++ JavaScript/smth new/управление HTML и обработка форм/script.js
@@ -1,15 +1,33 @@
-let Text = document.getElementById('Text');
-console.log(Text.id)
-Text.title = "Это уже новый текст"
-console.log(Text.title)
-Text.style.color = "yellow";
-Text.style.backgroundColor = "black";
-Text.innerHTML = "Hello <br> world!";
-document.getElementById('Text').style.color = "orange"; 
+//document.getElementById('main-form').addEventListener("submit", checkForm);
 
-//let spanS = document.getElementsByClassName('simple-text');
-let spans = document.getElementsByTagName('span');
-for (let i=0; i < spans.length; i++) {
-    console.log(spans[i].innerHTML);
+//function checkForm(event) {
+function checkForm(el) {
+  //event.preventDefault();
+  //let name = document.getElementById('name').value;
+    let name = el.name.value;
+    let pass = el.pass.value;
+    let repass = el.repass.value;
+    let state = el.state.value;
+    
+    let failure = "";
+
+    if (name == "" || pass == "" || repass == "" || state == "") 
+        failure = "Пожалуйста, заполните все поля.";
+    else if (name.length <= 1 || name.length >= 15)
+        failure = "Введите корректное имя.";
+    else if (pass.length <= 5)
+        failure = "Придумайте более надежный пароль.";
+    else if (pass != repass)
+        failure = "Пароли должны совпадать.";
+    else if (pass.split("@").length > 1 || repass.split("@").length > 1)
+        failure = "Пароль содержит недопустимый символ.";
+
+    if (failure != "") 
+        document.getElementById('error').innerHTML = failure;
+    else {
+        alert("Все данные введены корректно.");
+        window.location = 'https://ru.wiktionary.org/wiki/%D1%81%D1%82%D0%B8%D0%BB%D1%8C';
+    }
+
+    return false;
 }
-
